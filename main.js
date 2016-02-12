09356fdc339a2a2a5b01d2a9600eba6ebd673f18
Kathy Dieser <katdiese@gmail.com>
2016-02-12 13:05:20 -0700

    added cat object

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
+//"setters" are methods which allow values to be set outside of the object
+//"getters" are methods which allow the values to be retrieved once set
 var person = {
   legs: 2,
   eyes: 2,
   arms: 2,
-  name: "",
+  firstName: "",
+  lastName: "",
   greet: function() {
     console.log("Hello!");
   },
-  changeName: function(newName) {
-    this.name = newName;
+  setFirstName: function(firstName) {
+    this.firstName = firstName;
   },
-  getName: function() {
-    console.log(this.name);
+  setLastName: function(lastName) {
+    this.lastName = lastName;
+  },
+  getFullName: function() {
+    return this.firstName + " " + this.lastName;
   },
   addEyes: function(additionalEyes) {
     this.eyes += additionalEyes;
@@ -20,6 +26,39 @@ var person = {
   }
 }
 
-person.greet();
-person.addEyes(3);
-person.countEyes();
+
+//Those variables created with = signs within the createPlant function are private, and can not be accessed besides through the publically declared method plantStatus
+function createPlant() {
+  var color = "green";
+  var potted = true;
+  var flowered = false;
+  var growing = function() {
+    return "growing";
+  }
+  return {
+    plantStatus: function(numberOfDays) {
+      console.log("This plant was watered " + numberOfDays + " days ago and this plant is currently " + growing());
+    }
+  }
+}
+
+var roomPlant = createPlant();
+var roomPlant1 = new createPlant();
+
+//Inheritance
+//a Fruit is an object with hierarchical taxonomy, it inherits characteristics and methods
+//level 1: living thing: needs fuel, oxygen, water and it grows
+//level 2: plant: grows from a seed, converts sunlight to fuel, produces seeds
+//level 3: fruit: edible to humans, can be juiced, has a color
+
+var cat = function(catName) {
+  this.name = catName;
+  this.legs = 4;
+  this.meow = function() {
+    console.log("MEEEEOOOOWWWWWW");
+  }
+}
+
+var hoshi = new cat("Hoshi");
+hoshi.meow();
+
