004135120b4147ed7708d09674ec5c37e42832df
Kathy Dieser <katdiese@gmail.com>
2016-02-12 16:48:33 -0700

    input example of polymorphism

--- main.js
+++ main.js
@@ -51,6 +51,30 @@ var roomPlant1 = new createPlant();
 //level 2: plant: grows from a seed, converts sunlight to fuel, produces seeds
 //level 3: fruit: edible to humans, can be juiced, has a color
 
+var livingThing = function(name) {
+  this.name = name;
+  this.needsOxygen= true;
+  this.needsWater= true;
+  this.age = 0;
+  this.timePass= function() {
+    this.age += 1;
+    console.log("The age is now " + this.age);
+  }
+}
+
+var plant = new livingThing("Fruit Tree");
+plant.timePass();
+plant.needsOxygen = true;
+plant.needsWater = true;
+plant.beginning = "seed";
+
+var apple = new livingThing("Apple");
+apple.edible = true;
+apple.juiceable = true;
+apple.color = "red";
+
+
+
 var cat = function(catName) {
   this.name = catName;
   this.legs = 4;
@@ -61,4 +85,40 @@ var cat = function(catName) {
 
 var hoshi = new cat("Hoshi");
 hoshi.meow();
+
+//example of polymorphism, or overriding the method, but still retaining the same kind of output so that it is able to be used later in a function
+var reader = function() {
+  this.numberOfBooksRead = function(numberOfBooks) {
+    for(i = 0; i <=numberOfBooks; i++) {
+      console.log("Book conquered! ");
+    }
+  }
+}
+
+var slowReader = new reader();
+var regularReader = new reader();
+var fastReader = new reader();
+
+slowReader.numberOfBooksRead = function(numberOfWeeks) {
+  for(i = 0; i <= (numberOfWeeks/2); i++) {
+    console.log("Finished a book. ");
+  }
+}
+regularReader.numberOfBooksRead = function(numberOfWeeks) {
+  for(i = 0; i <= numberOfWeeks; i++) {
+    console.log("Finished a book. ");
+  }
+}
+fastReader.numberOfBooksRead = function(numberOfWeeks) {
+  for(i = 0; i <= (numberOfWeeks * 3); i++) {
+    console.log("Finished a book. ")
+  }
+}
+
+var howManyRead = function(reader, numberOfWeeks) {
+  return reader.numberOfBooksRead(numberOfWeeks);
+}
+
+howManyRead(slowReader, 7);
+
 
